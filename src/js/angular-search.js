6f9d1337718355421180998d9dc3324e72770d81
Sanchit <dev@minutephp.com>
2016-12-10 11:41:20 +0530

    updated to version v0.0.3

--- src/js/angular-search.js
+++ src/js/angular-search.js
@@ -3,31 +3,37 @@ var Minute;
 (function (Minute) {
     var AngularSearch = (function () {
         function AngularSearch() {
-            this.$get = function ($rootScope, $q, $timeout) {
+            this.$get = function ($rootScope, $q, $timeout, $http) {
                 var service = {};
                 var suggest = function (query, youtube) {
-                    var apiKey = 'AI39si7ZLU83bKtKd4MrdzqcjTVI3DK9FvwJR6a4kB_SW_Dbuskit-mEYqskkSsFLxN5DiG1OBzdHzYfW0zXWjxirQKyxJfdkg';
                     var deferred = $q.defer();
-                    var cancel = $timeout(deferred.resolve, 2500);
-                    $.ajax({
-                        url: "//suggestqueries.google.com/complete/search?hl=en&client=youtube&hjson=t&cp=1&q=" + encodeURIComponent(query) + "&key=" + apiKey + "&format=5&alt=json&callback=?" + (youtube ? '&ds=yt' : ''),
-                        dataType: 'jsonp',
-                        success: function (data) {
-                            $timeout.cancel(cancel);
-                            var results = $.map(data[1], function (item) {
-                                return item[0];
-                            });
-                            if (results && results.length > 2) {
-                                deferred.resolve(results);
-                            }
-                            else if (youtube === true) {
-                                var promise = suggest(query, false);
-                                promise.then(deferred.resolve);
-                            }
-                            else {
-                                deferred.resolve([]);
-                            }
+                    var cancel = $timeout(function () { return deferred.resolve([]); }, 2500);
+                    var apiKey = 'AI39si7ZLU83bKtKd4MrdzqcjTVI3DK9FvwJR6a4kB_SW_Dbuskit-mEYqskkSsFLxN5DiG1OBzdHzYfW0zXWjxirQKyxJfdkg';
+                    var yt = youtube ? '&ds=yt' : '';
+                    var url = '//suggestqueries.google.com/complete/search?hl=en&client=youtube&hjson=t&cp=1&q=' + encodeURIComponent(query) + '&key=' + apiKey + '&format=5&alt=json&callback=JSON_CALLBACK' + yt;
+                    $http.jsonp(url).then(function (obj) {
+                        $timeout.cancel(cancel);
+                        var results = $.map(obj.data[1], function (item) { return item[0]; });
+                        if (results && results.length > 2) {
+                            deferred.resolve(results);
                         }
+                        else if (youtube === true) {
+                            var promise = suggest(query, false);
+                            promise.then(function (results) { return deferred.resolve(results); });
+                        }
+                        else {
+                            deferred.resolve([]);
+                        }
+                    });
+                    return deferred.promise;
+                };
+                var stockSearch = function (type, query, params) {
+                    if (params === void 0) { params = ''; }
+                    var deferred = $q.defer();
+                    var cancel = $timeout(function () { return deferred.resolve([]); }, 5000);
+                    $http.jsonp('//www.stockutils.com/' + type + '/' + encodeURIComponent(query) + '?callback=JSON_CALLBACK' + params).then(function (results) {
+                        $timeout.cancel(cancel);
+                        deferred.resolve(results.data);
                     });
                     return deferred.promise;
                 };
@@ -37,34 +43,87 @@ var Minute;
                 service.googleSuggest = function (query) {
                     return suggest(query, false);
                 };
-                service.wikiSearch = function (query) {
+                service.wikiSearch = function (query, len) {
+                    if (len === void 0) { len = 0; }
                     var deferred = $q.defer();
-                    var cancel = $timeout(deferred.resolve, 2500);
-                    $.getJSON("//en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + encodeURIComponent(query) + "&callback=?", function (obj) {
-                        if (obj && obj.query && obj.query.pages) {
-                            var results = [];
-                            $timeout.cancel(cancel);
-                            for (var page in obj.query.pages) {
-                                if (obj.query.pages.hasOwnProperty(page)) {
-                                    var text = obj.query.pages[page].extract;
-                                    results.push(text);
+                    var cancel = $timeout(function () { return deferred.resolve(''); }, 5000);
+                    var ucQuery = (query || '').replace(/\b\w/g, function (l) { return l.toUpperCase(); });
+                    $http.jsonp('//en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + encodeURIComponent(ucQuery) + '&callback=JSON_CALLBACK').then(function (obj) {
+                        $timeout.cancel(cancel);
+                        for (var page in obj.data.query.pages) {
+                            if (obj.data.query.pages.hasOwnProperty(page)) {
+                                var text = obj.data.query.pages[page].extract;
+                                if (text && !/^Report generated based on a request/.test(text)) {
+                                    var output = '';
+                                    if (len > 0) {
+                                        var sentences = text.split(/[\.?!](?=\s+[0-9A-Z])/g);
+                                        for (var i = 0; i < sentences.length; i++) {
+                                            var sentence = $.trim(sentences[i].replace(/\s*\([^)]*\)\s*/g, " ").replace(/\s+/, ' '));
+                                            if ((output.length === 0) || ((output.length + sentence.length) < len + 20)) {
+                                                output = output + sentence + '. ';
+                                            }
+                                            else {
+                                                break;
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        output = text;
+                                    }
+                                    deferred.resolve((output || '').replace(/\W+$/, '.'));
+                                    break;
                                 }
                             }
-                            deferred.resolve(results);
                         }
                     });
                     return deferred.promise;
                 };
-                service.googleSearch = function (query) {
+                service.youtubeSearch = function (query, cc, duration, type) {
+                    if (cc === void 0) { cc = false; }
+                    if (duration === void 0) { duration = 'any'; }
+                    if (type === void 0) { type = 'video'; }
+                    var deferred = $q.defer();
+                    var cancel = $timeout(function () { return deferred.resolve(''); }, 5000);
+                    $http.get('https://www.googleapis.com/youtube/v3/search', {
+                        params: {
+                            q: query,
+                            key: 'AIzaSyCBdyG0mb-kaxrHkY3bH3LmPGqigFrEtVg',
+                            type: type,
+                            part: 'id,snippet',
+                            duration: duration,
+                            maxResults: '24',
+                            videoLicense: cc ? 'creativeCommon' : 'any',
+                            videoCategoryId: '10',
+                            videoEmbeddable: true
+                        }
+                    }).then(function (obj) {
+                        $timeout.cancel(cancel);
+                        var results = [];
+                        angular.forEach(obj.data.items, function (result) {
+                            results.push({
+                                src: 'http://www.youtube.com/watch?v=' + result.id.videoId,
+                                title: result.snippet.title,
+                                thumbnail: result.snippet.thumbnails.default.url,
+                                description: result.snippet.description
+                            });
+                        });
+                        deferred.resolve(results);
+                    });
+                    return deferred.promise;
                 };
-                service.imageSearch = function (query) {
+                service.googleSearch = function (query) {
+                    return stockSearch('web-search', query);
+                };
+                service.imageSearch = function (query, cc) {
+                    if (cc === void 0) { cc = true; }
+                    return stockSearch('image-search', query, cc ? '&cc=true' : '');
                 };
                 service.init = function () {
                     return service;
                 };
                 return service.init();
             };
-            this.$get.$inject = ['$rootScope', '$q', '$timeout'];
+            this.$get.$inject = ['$rootScope', '$q', '$timeout', '$http'];
         }
         return AngularSearch;
     }());
--- src/js/angular-search.ts
+++ src/js/angular-search.ts
@@ -3,35 +3,44 @@
 module Minute {
     export class AngularSearch implements ng.IServiceProvider {
         constructor() {
-            this.$get.$inject = ['$rootScope', '$q', '$timeout'];
+            this.$get.$inject = ['$rootScope', '$q', '$timeout', '$http'];
         }
 
-        $get = ($rootScope: ng.IRootScopeService, $q: ng.IQService, $timeout: ng.ITimeoutService) => {
+        $get = ($rootScope: ng.IRootScopeService, $q: ng.IQService, $timeout: ng.ITimeoutService, $http: ng.IHttpService) => {
             let service: any = {};
             let suggest = (query, youtube) => {
-                var apiKey = 'AI39si7ZLU83bKtKd4MrdzqcjTVI3DK9FvwJR6a4kB_SW_Dbuskit-mEYqskkSsFLxN5DiG1OBzdHzYfW0zXWjxirQKyxJfdkg';
-                var deferred = $q.defer();
-                var cancel = $timeout(deferred.resolve, 2500);
+                let deferred = $q.defer();
+                let cancel = $timeout(() => deferred.resolve([]), 2500);
 
-                $.ajax({
-                    url: "//suggestqueries.google.com/complete/search?hl=en&client=youtube&hjson=t&cp=1&q=" + encodeURIComponent(query) + "&key=" + apiKey + "&format=5&alt=json&callback=?" + (youtube ? '&ds=yt' : ''),
-                    dataType: 'jsonp',
-                    success: function (data) {
-                        $timeout.cancel(cancel);
+                let apiKey = 'AI39si7ZLU83bKtKd4MrdzqcjTVI3DK9FvwJR6a4kB_SW_Dbuskit-mEYqskkSsFLxN5DiG1OBzdHzYfW0zXWjxirQKyxJfdkg';
+                let yt = youtube ? '&ds=yt' : '';
+                let url = '//suggestqueries.google.com/complete/search?hl=en&client=youtube&hjson=t&cp=1&q=' + encodeURIComponent(query) + '&key=' + apiKey + '&format=5&alt=json&callback=JSON_CALLBACK' + yt;
 
-                        var results = $.map(data[1], function (item) {
-                            return item[0]
-                        });
+                $http.jsonp(url).then((obj) => {
+                    $timeout.cancel(cancel);
 
-                        if (results && results.length > 2) {
-                            deferred.resolve(results);
-                        } else if (youtube === true) { //return normal suggest results if youtube doesn't return anything
-                            var promise = suggest(query, false);
-                            promise.then(deferred.resolve);
-                        } else {
-                            deferred.resolve([]);
-                        }
+                    let results = $.map(obj.data[1], (item) => item[0]);
+
+                    if (results && results.length > 2) {
+                        deferred.resolve(results);
+                    } else if (youtube === true) { //return normal suggest results if youtube doesn't return anything
+                        var promise = suggest(query, false);
+                        promise.then((results) => deferred.resolve(results));
+                    } else {
+                        deferred.resolve([]);
                     }
+                });
+
+                return deferred.promise;
+            };
+
+            let stockSearch = (type, query, params = '') => {
+                let deferred = $q.defer();
+                let cancel = $timeout(() => deferred.resolve([]), 5000);
+
+                $http.jsonp('//www.stockutils.com/' + type + '/' + encodeURIComponent(query) + '?callback=JSON_CALLBACK' + params).then((results) => {
+                    $timeout.cancel(cancel);
+                    deferred.resolve(results.data);
                 });
 
                 return deferred.promise;
@@ -45,37 +54,90 @@ module Minute {
                 return suggest(query, false);
             };
 
-            service.wikiSearch = (query) => {
+            service.wikiSearch = (query, len = 0) => {
                 let deferred = $q.defer();
-                let cancel = $timeout(deferred.resolve, 2500);
+                let cancel = $timeout(() => deferred.resolve(''), 5000);
+                let ucQuery = (query || '').replace(/\b\w/g, l => l.toUpperCase());
 
-                $.getJSON("//en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=" + encodeURIComponent(query) + "&callback=?", function (obj) {
-                    if (obj && obj.query && obj.query.pages) {
-                        let results = [];
+                $http.jsonp('//en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=' + encodeURIComponent(ucQuery) + '&callback=JSON_CALLBACK').then((obj: any) => {
+                    $timeout.cancel(cancel);
 
-                        $timeout.cancel(cancel);
+                    for (let page in obj.data.query.pages) {
+                        if (obj.data.query.pages.hasOwnProperty(page)) {
+                            let text = obj.data.query.pages[page].extract;
 
-                        for (var page in obj.query.pages) {
-                            if (obj.query.pages.hasOwnProperty(page)) {
-                                var text = obj.query.pages[page].extract;
+                            if (text && !/^Report generated based on a request/.test(text)) {
+                                let output = '';
 
-                                results.push(text);
+                                if (len > 0) {
+                                    let sentences = text.split(/[\.?!](?=\s+[0-9A-Z])/g);
+
+                                    for (let i = 0; i < sentences.length; i++) {
+                                        let sentence = $.trim(sentences[i].replace(/\s*\([^)]*\)\s*/g, " ").replace(/\s+/, ' '));
+
+                                        if ((output.length === 0) || ((output.length + sentence.length) < len + 20)) {
+                                            output = output + sentence + '. ';
+                                        } else {
+                                            break;
+                                        }
+                                    }
+                                } else {
+                                    output = text;
+                                }
+
+                                deferred.resolve((output || '').replace(/\W+$/, '.'));
+                                break;
                             }
                         }
-
-                        deferred.resolve(results);
                     }
                 });
 
                 return deferred.promise;
             };
 
-            service.googleSearch = (query) => {
+            service.youtubeSearch = (query, cc = false, duration = 'any', type = 'video') => {
+                let deferred = $q.defer();
+                let cancel = $timeout(() => deferred.resolve(''), 5000);
 
+                $http.get('https://www.googleapis.com/youtube/v3/search', {
+                    params: {
+                        q: query,
+                        key: 'AIzaSyCBdyG0mb-kaxrHkY3bH3LmPGqigFrEtVg',
+                        type: type,
+                        part: 'id,snippet',
+                        duration: duration,
+                        maxResults: '24',
+                        videoLicense: cc ? 'creativeCommon' : 'any',
+                        videoCategoryId: '10',
+                        videoEmbeddable: true
+                    }
+                }).then(function (obj: any) {
+                    $timeout.cancel(cancel);
+
+                    let results = [];
+
+                    angular.forEach(obj.data.items, function (result) {
+                        results.push({
+                            src: 'http://www.youtube.com/watch?v=' + result.id.videoId,
+                            title: result.snippet.title,
+                            thumbnail: result.snippet.thumbnails.default.url,
+                            description: result.snippet.description,
+                        });
+                    });
+
+                    deferred.resolve(results);
+                });
+
+
+                return deferred.promise;
             };
 
-            service.imageSearch = (query) => {
+            service.googleSearch = (query) => {
+                return stockSearch('web-search', query);
+            };
 
+            service.imageSearch = (query, cc = true) => {
+                return stockSearch('image-search', query, cc ? '&cc=true' : '');
             };
 
             service.init = () => {
